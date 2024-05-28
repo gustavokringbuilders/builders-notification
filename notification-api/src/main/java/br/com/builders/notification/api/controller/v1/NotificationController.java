a6a3ed9c32a705370b5f80d496e4b34bc53b9e55
gustavokringbuilders <gustavo.kring@platformbuilders.io>
2024-05-28 15:14:58 -0300

    ajuste endpoint faltou mapeamento de parametro

--- notification-api/src/main/java/br/com/builders/notification/api/controller/v1/NotificationController.java
+++ notification-api/src/main/java/br/com/builders/notification/api/controller/v1/NotificationController.java
@@ -7,10 +7,7 @@ import br.com.builders.notification.core.modules.notification.service.Notificati
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,7 +22,8 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping(produces = MediaType.APPLICATION_NDJSON_VALUE)
-    public Flux<NotificationDTO> findNotifications(final LocalDateTime from, final LocalDateTime to) {
+    public Flux<NotificationDTO> findNotifications(@RequestParam final LocalDateTime from,
+                                                   @RequestParam final LocalDateTime to) {
         return notificationService.findByDateInterval(from, to)
                 .map(NotificationMapper::parseTo);
     }
